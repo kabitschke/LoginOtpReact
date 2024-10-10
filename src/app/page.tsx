a213416f230195c4aa5e5f432961fd2f3d30e102
Maycon Kabitschke <mayconkabitschke@gmail.com>
2024-10-10 03:06:36 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,100 @@
-import Image from "next/image";
+"use client"
 
-export default function Home() {
+import React, { useState, useEffect, useRef } from 'react';
+
+const Home: React.FC = () => {
+  const [otp, setOtp] = useState<Array<number | undefined>>(Array(6).fill(undefined));
+  const inputsRef = useRef<(HTMLInputElement | null)[]>([]);
+
+ 
+
+  useEffect(() => {
+    inputsRef.current[0]?.focus();
+  }, []);
+
+  const handleInput = (event: React.ChangeEvent<HTMLInputElement>, index: number) => {
+    const inputValue = event.target.value;
+
+    if (inputValue && !isNaN(Number(inputValue))) {
+      const updatedOtp = [...otp];
+      updatedOtp[index] = Number(inputValue);
+      setOtp(updatedOtp);
+
+      if (index < 5) {
+        inputsRef.current[index + 1]?.focus();
+      }
+    } else {
+      const updatedOtp = [...otp];
+      updatedOtp[index] = undefined;
+      setOtp(updatedOtp);
+    }
+  };
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>, index: number) => {
+    if (event.key === 'Backspace') {
+      const updatedOtp = [...otp];
+      updatedOtp[index] = undefined;
+      setOtp(updatedOtp);
+
+      // Foca no campo anterior, se não for o primeiro
+      if (index > 0 && !otp[index]) {
+        inputsRef.current[index - 1]?.focus();
+      }
+    }
+  };
+
+  const handlePaste = (event: React.ClipboardEvent<HTMLInputElement>, index: number) => {
+    event.preventDefault();
+    const pasteContent = event.clipboardData.getData('text').trim();
+    const updatedOtp = [...otp];
+
+    for (let i = 0; i < pasteContent.length && i < 6; i++) {
+      const num = parseInt(pasteContent[i]);
+      if (!isNaN(num)) {
+        updatedOtp[i] = num;
+      }
+    }
+    setOtp(updatedOtp);
+    inputsRef.current[Math.min(pasteContent.length, 5)]?.focus();
+  };
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <>
+      <header className="bg-purple-500 p-4">
+        <div className="container">
+          <h1 className="text-white text-2xl">Sistema de login OTP</h1>
         </div>
+      </header>
+
+      <main className="container">
+        <h2 className="mt-4">Digite o código</h2>
+        <p>Te enviamos um código no e-mail, digite-o aqui.</p>
+
+        <div className="otp-area mt-4 text-black">
+          {otp.map((value, index) => (
+            <input
+              key={index}
+              type="text"
+              value={value !== undefined ? value : ''}
+              onChange={(event) => handleInput(event, index)}
+              onKeyDown={(event) => handleKeyDown(event, index)} // Adicione o tratamento de KeyDown
+              onPaste={(event) => handlePaste(event, index)}
+              ref={(el) => {
+                inputsRef.current[index] = el;
+              }}
+              maxLength={1}
+              className="otp-input"
+            />
+          ))}
+        </div>
+
+        <button className="btn">Confirmar código</button>
+        <p className="mt-4">
+          Não recebeu o código? <a href="#">Enviar novamente</a>
+        </p>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    </>
   );
-}
+};
+
+export default Home;
